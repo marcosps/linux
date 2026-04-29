767fb996246684304ee48138ef17968bb3894cda
Marcos Paulo de Souza <mpdesouza@suse.com>
2026-04-29 15:35:29 -0300

    selftests: livepatch: Introduce _remove_mod function
    This new function will be used in the next patch to remove a loaded
    modules when a testcase fails.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- tools/testing/selftests/livepatch/functions.sh
+++ tools/testing/selftests/livepatch/functions.sh
@@ -241,9 +241,10 @@ function load_failing_mod() {
 	log "$ret"
 }
 
-# unload_mod(modname) - unload a kernel module
+# _remove_mod(modname) - Remove a loaded module, should be called from
+#                        functions.sh only.
 #	modname - module name to unload
-function unload_mod() {
+function _remove_mod() {
 	local mod="$1"
 
 	# Wait for module reference count to clear ...
@@ -259,6 +260,14 @@ function unload_mod() {
 	# Wait for module in sysfs ...
 	loop_until '[[ ! -e "/sys/module/$mod" ]]' ||
 		die "failed to unload module $mod (/sys/module)"
+}
+
+# unload_mod(modname) - unload a kernel module
+#	modname - module name to unload
+function unload_mod() {
+	local mod="$1"
+
+	_remove_mod "$mod"
 }
 
 # unload_lp(modname) - unload a kernel module with a livepatch
